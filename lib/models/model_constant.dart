62a1d6b61cd8a58ccb4df483d5690ee3f2fccbc6
Abhinandan Mandal <abhinandan.swadesh@gmail.com>
2022-08-31 13:51:36 +0530

    Updated Api Design

--- lib/models/model_constant.dart
+++ lib/models/model_constant.dart
@@ -1,4 +1,4 @@
-//-------------CategoryModel--------------------------------
+//------------------1.CategoryModel--------------------------------
 
 // ignore_for_file: non_constant_identifier_names
 
@@ -7,34 +7,38 @@ class CategoryApiModel {
   final List<CategoryData> dataList;
 
   CategoryApiModel(this.status, this.msg, this.dataList);
-  
-  factory CategoryApiModel.formJson(Map<String,dynamic> myjson){
+
+  factory CategoryApiModel.formJson(Map<String, dynamic> myjson) {
     List list = myjson['data'];
-    return CategoryApiModel(myjson['status'], myjson['msg'], List<CategoryData>.from(list.map((e) => CategoryData.formJson(e))));
+    return CategoryApiModel(myjson['status'], myjson['msg'],
+        List<CategoryData>.from(list.map((e) => CategoryData.formJson(e))));
   }
 }
 
 class CategoryData {
-  // ignore: non_constant_identifier_names
   final int Cat_Id;
-  final String Cat_Name,ImgUrl;
+  final String Cat_Name, ImgUrl;
 
   CategoryData(this.Cat_Id, this.Cat_Name, this.ImgUrl);
   factory CategoryData.formJson(Map<String, dynamic> myjson) {
-    return CategoryData(myjson['Cat_Id'], myjson['Cat_Name'],myjson['ImgUrl']);
+    return CategoryData(myjson['Cat_Id'], myjson['Cat_Name'], myjson['ImgUrl']);
   }
 }
 
-//------------------CategoryColor Model --------------------
+//------------------2.CategoryColor Model --------------------
 
-class CategoryColorModel{
-  final String status,msg;
+class CategoryColorModel {
+  final String status, msg;
   final List<CategoryColorData> dataList1;
 
   CategoryColorModel(this.status, this.msg, this.dataList1);
-  factory CategoryColorModel.formJson(Map<String,dynamic> myjson){
-     List list1 = myjson['data'];
-    return CategoryColorModel(myjson['status'], myjson['msg'], List<CategoryColorData>.from(list1.map((e) => CategoryColorData.formJson(e))));
+  factory CategoryColorModel.formJson(Map<String, dynamic> myjson) {
+    List list1 = myjson['data'];
+    return CategoryColorModel(
+        myjson['status'],
+        myjson['msg'],
+        List<CategoryColorData>.from(
+            list1.map((e) => CategoryColorData.formJson(e))));
   }
 }
 
@@ -43,7 +47,100 @@ class CategoryColorData {
   final String Color_Name;
 
   CategoryColorData(this.Color_Id, this.Color_Name);
-  factory CategoryColorData.formJson(Map<String, dynamic> myjson){
+  factory CategoryColorData.formJson(Map<String, dynamic> myjson) {
     return CategoryColorData(myjson['Color_Id'], myjson['Color_Name']);
   }
+}
+
+//---------------------3.CategoryAginstSubcategory Model--------------------
+
+// class CategoryListModel {
+//   final int Cat_Id;
+//   final String Cat_Name;
+//   final List<subcategorylistModel> subcategorylistModelData;
+//   CategoryListModel(this.Cat_Id, this.Cat_Name, this.subcategorylistModelData);
+//   factory CategoryListModel.formJson(Map<String, dynamic> myjson) {
+//     List list = myjson['data'];
+//     return CategoryListModel(
+//         myjson['Cat_Id'],
+//         myjson['Cat_Name'],
+//         List<subcategorylistModel>.from(
+//             list.map((e) => subcategorylistModel.formJson(e))));
+//   }
+// }
+
+// class subcategorylistModel {
+//   final int Sub_Id;
+//   final String SubName, IsActive;
+
+//   subcategorylistModel(this.Sub_Id, this.SubName, this.IsActive);
+//   factory subcategorylistModel.formJson(Map<String, dynamic> myjson) {
+//     return subcategorylistModel(
+//         myjson['Sub_Id'], myjson['SubName'], myjson['IsActive']);
+//   }
+// }
+
+
+class Welcome {
+    Welcome({
+        required this.categoryList,
+    });
+
+    List<CategoryList> categoryList;
+
+    factory Welcome.fromJson(Map<String, dynamic> json) => Welcome(
+        categoryList: List<CategoryList>.from(json["CategoryList"].map((x) => CategoryList.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "CategoryList": List<dynamic>.from(categoryList.map((x) => x.toJson())),
+    };
+}
+
+class CategoryList {
+    CategoryList({
+        required this.catId,
+        required this.catName,
+        required this.subcategorylist,
+    });
+
+    String catId;
+    String catName;
+    List<Subcategorylist> subcategorylist;
+
+    factory CategoryList.fromJson(Map<String, dynamic> json) => CategoryList(
+        catId: json["Cat_Id"],
+        catName: json["Cat_Name"],
+        subcategorylist: List<Subcategorylist>.from(json["subcategorylist"].map((x) => Subcategorylist.fromJson(x))),
+    );
+
+    Map<String, dynamic> toJson() => {
+        "Cat_Id": catId,
+        "Cat_Name": catName,
+        "subcategorylist": List<dynamic>.from(subcategorylist.map((x) => x.toJson())),
+    };
+}
+
+class Subcategorylist {
+    Subcategorylist({
+        required this.subId,
+        required this.subName,
+        required this.isActive,
+    });
+
+    String subId;
+    String subName;
+    String isActive;
+
+    factory Subcategorylist.fromJson(Map<String, dynamic> json) => Subcategorylist(
+        subId: json["Sub_Id"],
+        subName: json["SubName"],
+        isActive: json["IsActive"],
+    );
+
+    Map<String, dynamic> toJson() => {
+        "Sub_Id": subId,
+        "SubName": subName,
+        "IsActive": isActive,
+    };
 }
--- lib/views/all_categories/electronic.dart
+++ lib/views/all_categories/electronic.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+import 'package:ecommerce_website_logo3_8_22/models/model_constant.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:http/http.dart';
 
 class Electronic extends StatefulWidget {
   const Electronic({Key? key}) : super(key: key);
@@ -8,8 +12,54 @@ class Electronic extends StatefulWidget {
 }
 
 class _ElectronicState extends State<Electronic> {
+  List<Subcategorylist> subcategorylistModelList = [];
+  List<CategoryList> categoryList1 = [];
+  // late Future<List<Welcome>> _future;
+  @override
+  void initState() {
+    super.initState();
+    _getPost();
+  }
+
+  _getPost() async {
+    String url =
+        'https://demo50.gowebbi.us/api/MasterApi/CategoryAginstSubcategory';
+    var result = await get(Uri.parse(url));
+    if (result.statusCode == 200) {
+      var response = Welcome.fromJson(jsonDecode(result.body));
+      categoryList1 = response.categoryList;
+      // ignore: avoid_print
+      setState(() {});
+      // ignore: avoid_print
+      print(result.body);
+    } else {
+      // ignore: avoid_print
+      print('Api error ${result.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          height: 0.5.sh,
+          child: GridView.builder(
+              itemCount: categoryList1.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 3,
+                childAspectRatio: 0.7,
+              ),
+              itemBuilder: (context, index) {
+                return Column(
+                  children: [
+                    Text(categoryList1[index].catId),
+                    Text(categoryList1[index].catName),
+                  ],
+                );
+              }),
+        ),
+      ),
+    );
   }
 }
